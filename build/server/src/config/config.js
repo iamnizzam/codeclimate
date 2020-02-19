71e8a806655e1e9fa833b6812cfb3a55938e96f0
iamnizzam <iamnizzam@yahoo.com>
2020-02-19 18:40:33 +0600

    server updated

---
+++ build/server/src/config/config.js
@@ -0,0 +1,32 @@
+"use strict";
+
+require('dotenv').config();
+
+module.exports = {
+  // If using onine database
+  // development: {
+  //   use_env_variable: 'DATABASE_URL'
+  // },
+  development: {
+    database: 'books',
+    username: 'sjfxxizz',
+    password: 'wNwuUDSHbRhNO7M3PGfpdb7pF3sy1KAS',
+    host: 'arjuna.db.elephantsql.com',
+    dialect: 'postgres'
+  },
+  test: {
+    database: 'books',
+    username: 'sjfxxizz',
+    password: 'wNwuUDSHbRhNO7M3PGfpdb7pF3sy1KAS',
+    host: 'arjuna.db.elephantsql.com',
+    dialect: 'postgres'
+  },
+  production: {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  }
+};
+//# sourceMappingURL=config.js.map
